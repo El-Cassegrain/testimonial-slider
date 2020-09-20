4d8658405b9538134f404d6b1c673e04a03e3094
El-Cassegrain <etienneleriche@gmail.com>
2020-09-20 14:25:05 +0200

    final product

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+/* Script */
+$('.slick-container').slick({
+    prevArrow: '<button type="button" class="slick-prev"><i class="icon-prev"><img src="assets/img/icon-prev.svg" /></i></button>',
+    nextArrow: '<button type="button" class="slick-next"><i class="icon-next"><img src="assets/img/icon-next.svg" /></i></button>'
+});
